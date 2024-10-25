d5c70d2bb6bd2c80dc8fdb61effd9258bf6567ca
harsh <harsh.verlead@gmail.com>
2024-10-25 23:15:59 +0530

    added new file demoClass

---
+++ CucumberProject/src/test/java/pageobjects/demoClass.java
@@ -0,0 +1,11 @@
+package pageobjects;
+
+public class demoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is new class file");
+	}
+
+}
